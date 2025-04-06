f8de7026373b34729a06afa7434d11f517176a72
Kamikaze021 <pradothierry36@gmail.com>
2025-04-06 15:32:20 -0300

    ajuste de porta para deploy na Render

--- app.py
+++ app.py
@@ -57,7 +57,10 @@ def pagar(indice):
     salvar_dados(alunos)
     return redirect(url_for('index'))
 
+import os
+
 if __name__ == '__main__':
-    print("Iniciando servidor...")
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
 
